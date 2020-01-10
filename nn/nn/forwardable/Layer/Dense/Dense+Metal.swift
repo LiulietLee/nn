452e0038fbf697e22f5002afca862648ea955edc
LiulietLee <lee.liuliet@hotmail.com>
2020-01-10 23:16:48 +0800

    dense batch metal

---
+++ nn/nn/forwardable/Layer/Dense/Dense+Metal.swift
@@ -0,0 +1,119 @@
+//
+//  Dense+Metal.swift
+//  nn
+//
+//  Created by Liuliet.Lee on 22/12/2019.
+//  Copyright © 2019 Liuliet.Lee. All rights reserved.
+//
+
+import Foundation
+import MetalPerformanceShaders
+
+extension Dense {
+    
+    private func matrixMul(matrix: NNArray, input: NNArray) {
+        let pipeline = Core.pipeline(by: "dense_matrix_mul");
+        let queue = Core.queue()
+        
+        let commandBuffer = queue.makeCommandBuffer()!
+        let w = min(batchSize, pipeline.threadExecutionWidth)
+        let h = min(outFeatures, pipeline.maxTotalThreadsPerThreadgroup / w)
+        let d = min(inFeatures, pipeline.maxTotalThreadsPerThreadgroup / w / h)
+
+        Core.encode(
+            commandBuffer: commandBuffer,
+            pipeline: pipeline,
+            buffers: Core.buffer(input), Core.buffer(&outFeatures),  Core.buffer(&inFeatures), Core.buffer(matrix),
+            grid: [batchSize, outFeatures, inFeatures],
+            thread: [w, h, d]
+        )
+                
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+    }
+    
+    private func matrixSum(matrix: NNArray) {
+        let pipeline = Core.pipeline(by: "dense_matrix_sum");
+        let queue = Core.queue()
+        
+        let commandBuffer = queue.makeCommandBuffer()!
+        let w = min(batchSize, pipeline.threadExecutionWidth)
+        let h = min(outFeatures, pipeline.maxTotalThreadsPerThreadgroup / w)
+        
+        Core.encode(
+            commandBuffer: commandBuffer,
+            pipeline: pipeline,
+            buffers: Core.buffer(matrix), Core.buffer(&inFeatures), Core.buffer(&outFeatures), Core.buffer(bias), Core.buffer(&relu), Core.buffer(score), Core.buffer(interScore),
+            grid: [batchSize, outFeatures, 1],
+            thread: [w, h, 1]
+        )
+                
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+    }
+    
+    func forwardWithMetal(_ input: NNArray) {
+        let cp = NNArray(batchSize, outFeatures, inFeatures)
+        let size = MemoryLayout<Float>.stride * outFeatures * inFeatures
+        for i in 0..<batchSize {
+            memcpy(cp.data.pointer.advanced(by: size * i), param.data.pointer, size)
+        }
+        matrixMul(matrix: cp, input: input)
+        matrixSum(matrix: cp)
+    }
+}
+
+extension Dense {
+    
+    private func backward1(_ da: NNArray, _ delta: NNArray) {
+        // let row = outFeatures, col = inFeatures
+        let pipeline = Core.pipeline(by: "dense_backward_1");
+        let queue = Core.queue()
+        
+        let commandBuffer = queue.makeCommandBuffer()!
+        let w = min(batchSize, pipeline.threadExecutionWidth)
+        let h = min(inFeatures, pipeline.maxTotalThreadsPerThreadgroup / w)
+
+        Core.encode(
+            commandBuffer: commandBuffer,
+            pipeline: pipeline,
+            buffers: Core.buffer(&relu), Core.buffer(&outFeatures), Core.buffer(&inFeatures), Core.buffer(param), Core.buffer(delta), Core.buffer(interScore), Core.buffer(da),
+            grid: [batchSize, inFeatures, 1],
+            thread: [w, h, 1]
+        )
+
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+    }
+    
+    private func backward2(_ input: NNArray, _ delta: NNArray) {
+        // let row = outFeatures, col = inFeatures
+        let pipeline = Core.pipeline(by: "dense_backward_2");
+        let queue = Core.queue()
+        
+        let commandBuffer = queue.makeCommandBuffer()!
+        let w = min(batchSize, pipeline.threadExecutionWidth)
+        let h = min(outFeatures, pipeline.maxTotalThreadsPerThreadgroup / w)
+        let d = min(inFeatures, pipeline.maxTotalThreadsPerThreadgroup / w / h)
+
+        Core.encode(
+            commandBuffer: commandBuffer,
+            pipeline: pipeline,
+            buffers: Core.buffer(&relu), Core.buffer(&outFeatures),  Core.buffer(&inFeatures), Core.buffer(delta), Core.buffer(input), Core.buffer(interScore), Core.buffer(dparam), Core.buffer(dbias),
+            grid: [batchSize, outFeatures, inFeatures],
+            thread: [w, h, d]
+        )
+                
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+    }
+    
+    func backwardWithMetal(_ input: NNArray, _ delta: NNArray) -> NNArray {
+        let da = NNArray(input.count)
+
+        backward1(da, delta)
+        backward2(input, delta)
+        
+        return da
+    }
+}
---
+++ nn/nn/forwardable/Layer/Dense/dense_m.metal
@@ -0,0 +1,108 @@
+//
+//  dense_m.metal
+//  nn
+//
+//  Created by Liuliet.Lee on 22/12/2019.
+//  Copyright © 2019 Liuliet.Lee. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void dense_matrix_mul(device const float *input,
+                             device const int &out_features,
+                             device const int &in_features,
+                             device float *matrix,
+                             uint3 gid [[ thread_position_in_grid ]])
+{
+    // gid = [batch, outFeatures, inFeatures]
+    matrix[gid[0] * out_features * in_features +
+           gid[1] * in_features +
+           gid[2]]
+    *=
+    input[gid[0] * in_features +
+          gid[2]];
+}
+
+kernel void dense_matrix_sum(device const float *matrix,
+                             device const int &in_features,
+                             device const int &out_features,
+                             device const float *bi,
+                             device const bool &need_relu,
+                             device float *score,
+                             device float *inter_score,
+                             uint2 gid [[ thread_position_in_grid ]])
+{
+    // gid = [batch, outFeatures]
+    int start = gid[0] * in_features * out_features + gid[1] * in_features;
+    int index = gid[0] * out_features + gid[1];
+    inter_score[index] = 0.0;
+    
+    for (int i = 0; i < in_features; i++) {
+        inter_score[index] += matrix[start + i];
+    }
+
+    score[index] = inter_score[index];
+    if (need_relu && inter_score[index] < 0.0) {
+        score[index] *= 0.001;
+    }
+    
+    score[index] += bi[gid[1]];
+}
+
+kernel void dense_backward_1(device const bool &need_relu,
+                             device const int &out_features,
+                             device const int &in_features,
+                             device const float *matrix,
+                             device const float *delta,
+                             device const float *inter_score,
+                             device float *da,
+                             uint2 gid [[ thread_position_in_grid ]])
+{
+    // gid = [batch, inFeatures]
+    int index = gid[0] * in_features + gid[1];
+    da[index] = 0.0;
+    for (int i = 0; i < out_features; i++) {
+        float d =
+        delta[gid[0] * out_features +
+              i]
+        *
+        matrix[gid[0] * out_features * in_features +
+               gid[1] * in_features +
+               gid[2]];
+        
+        if (need_relu && inter_score[gid[0] * out_features + i] < 0.0) {
+            da[index] += d * 0.001;
+        } else {
+            da[index] += d;
+        }
+    }
+}
+
+kernel void dense_backward_2(device const bool &need_relu,
+                             device const int &out_features,
+                             device const int &in_features,
+                             device const float *delta,
+                             device const float *input,
+                             device const float *inter_score,
+                             device float *dparam,
+                             device float *dbias,
+                             uint3 gid [[ thread_position_in_grid ]])
+{
+    // gid = [batch, outFeatures, inFeatures]
+    int batch = gid[0];
+    int i = gid[1];
+    int j = gid[2];
+    
+    if (j == 0) {
+        dbias[batch * out_features + i] += delta[batch * out_features + i];
+    }
+    
+    float d = delta[batch * out_features + i] * input[batch * in_features + j];
+    int index = batch * in_features * out_features + i * out_features + j;
+    if (need_relu && inter_score[batch * out_features + i] < 0.0) {
+        dparam[index] += d * 0.001;
+    } else {
+        dparam[index] += d;
+    }
+}
